32e0923598cfbdb37b402ac6f310e819fd2c6630
Beyang Liu <beyang.liu@gmail.com>
2014-04-09 18:16:55 -0700

    sample code

---
+++ pkg0/m0.py
@@ -0,0 +1,30 @@
+"""
+Basic classes, methods, functions
+"""
+
+import os
+
+class Class0(object):
+    """Class0 docstring"""
+    var0 = 0
+    def meth0(self): pass
+
+class Class1(object):
+    def __init__(self):
+        self.var0 = 0
+        self.var0 = []
+        self.var1 = None
+
+    def meth0(self):
+        var0 = Class0()
+        self.var1 = var0
+
+class Class0_0(Class0):
+    def __init__(self, arg0, arg1, *args, **kwargs):
+        super(Class0_0, self).meth0()
+
+def f0():
+    var0 = Class0()
+    var1 = Class1()
+    var2 = Class0_0(1, 2)
+    return (var0, var1, var2)
---
+++ pkg0/m1.py
@@ -0,0 +1,15 @@
+"""
+Imports a module in same directory
+"""
+
+import pkg0
+from pkg0.m0 import Class0
+
+class Class0_0(Class0):
+    def __init__(self):
+        super(Class0_0, self).__init__()
+
+def m2_fn1():
+    var0 = Class0()
+    var1 = Class0_0()
+    return pkg0.m0.Class0_0()
