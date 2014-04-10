353aa7e690cb569ef14712500977c8a5b8fe1ab3
Beyang Liu <beyang.liu@gmail.com>
2014-04-09 18:24:27 -0700

    add setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='python-sample-0',
+    version='0.0.0',
+    url='http://github.com/sgtest/python-sample-0/',
+    packages=['pkg0'],
+    install_requires=[],
+)
